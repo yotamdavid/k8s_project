5425c5a1daedce5dee720ef8508beb079cebbbda
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-07-20 15:44:34 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,11 +3,17 @@ set -ex
 
 sleep 20
 
-# Get the current cluster IP
-CLUSTER_IP=$(kubectl cluster-info | grep 'Kubernetes control plane is running at' | awk '{print $NF}')
+# Get the LoadBalancer IP address
+CLUSTER_IP=$(kubectl get service docker-app-service -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
-# Run your tests using the cluster IP
-timeout 20 curl -s --cluster-ip $CLUSTER_IP
+# Check if CLUSTER_IP is empty, if it is, exit the script
+if [ -z "$CLUSTER_IP" ]; then
+    echo "Error: LoadBalancer IP not found. Exiting..."
+    exit 1
+fi
+
+# Run your tests using the LoadBalancer IP
+timeout 20 curl -s "http://$CLUSTER_IP:5000/"
 
 # <-- שורה מעודכנת -->
 echo "Test finished successfully"  # פלט מובנה לפלט של Jenkins
