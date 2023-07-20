84e3c88f3b4b89685a43e2c1cb322dbab0adee93
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-07-20 12:01:12 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,12 @@
 set -ex
 
 sleep 20
-# test1
-timeout 20 curl -s http://35.239.81.103:5000/
+
+# Get the current cluster IP
+CLUSTER_IP=$(kubectl cluster-info | grep 'Kubernetes control plane is running at' | awk '{print $NF}')
+
+# Run your tests using the cluster IP
+timeout 20 curl -s --cluster-ip $CLUSTER_IP
 
 # <-- שורה מעודכנת -->
 echo "Test finished successfully"  # פלט מובנה לפלט של Jenkins
