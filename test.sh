f74b861719f3452cf4b40e4fed071233f0a6de5d
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-07-23 14:04:37 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/bash
 set -ex
 
-sleep 30
+# Function to check if the LoadBalancer IP address is available
+check_loadbalancer_ip() {
+    CLUSTER_IP=$(kubectl get service docker-app-service -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    if [ -z "$CLUSTER_IP" ]; then
+        echo "LoadBalancer IP not found. Retrying in 5 seconds..."
+        sleep 5
+        check_loadbalancer_ip
+    fi
+}
 
-# Get the LoadBalancer IP address
-CLUSTER_IP=$(kubectl get service docker-app-service -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
-
-# Check if CLUSTER_IP is empty, if it is, exit the script
-if [ -z "$CLUSTER_IP" ]; then
-    echo "Error: LoadBalancer IP not found. Exiting..."
-    exit 1
-fi
+# Wait for the LoadBalancer IP address
+check_loadbalancer_ip
 
 # Run your tests using the LoadBalancer IP
 timeout 20 curl -s "http://$CLUSTER_IP:5000/"
 
-# <-- שורה מעודכנת -->
-echo "Test finished successfully"  # פלט מובנה לפלט של Jenkins
+echo "Test finished successfully"
