8a88d735e38081fa1068b0f57a1b960ce6e0f081
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-11 13:28:16 +0300

    Update update_db.sh

--- update_db.sh
+++ update_db.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Get the pod name of MySQL
-pod_name=$(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}')
-
 while true; do
+    pod_name=$(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}')
     # Exec into the pod and run the SQL commands
     kubectl exec -it "$pod_name" -- /bin/sh -c 'mysql -u root -pyotam -e "CREATE DATABASE IF NOT EXISTS users; USE users; CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50) NOT NULL, password_hash VARCHAR(255) NOT NULL);"'
 
