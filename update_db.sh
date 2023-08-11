9de137837a10e98c08143c093b9adc8ca4c81ade
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-11 12:29:11 +0300

    Update update_db.sh

--- update_db.sh
+++ update_db.sh
@@ -3,11 +3,5 @@
 # Get the pod name of MySQL
 pod_name=$(kubectl get pods -l app=mysql -o jsonpath='{.items[0].metadata.name}')
 
-# Copy the init.sql file into the pod
-kubectl cp /home/yotam/Desktop/k8s/k8s_project/init.sql "$pod_name":/tmp/init.sql
-
 # Exec into the pod and run the SQL commands
-kubectl exec -it "$pod_name" -- mysql -u root -pyotam users < /tmp/init.sql
-
-# Clean up the copied file from the pod
-kubectl exec -it "$pod_name" -- rm /tmp/init.sql
+kubectl exec -it "$pod_name" -- /bin/sh -c 'mysql -u root -pyotam -e "CREATE DATABASE IF NOT EXISTS users; USE users; CREATE TABLE IF NOT EXISTS users (id INT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(50) NOT NULL, password_hash VARCHAR(255) NOT NULL);"'
