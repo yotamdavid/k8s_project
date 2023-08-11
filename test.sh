3d21aaa74d0fa11548988f04f664db1297c7f548
yotamdavid <134198738+yotamdavid@users.noreply.github.com>
2023-08-11 13:49:04 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -ex
 
 # Function to check if all pods are ready
 check_pods_ready() {
-    PODS_READY=$(kubectl get pods -l app=docker-app -o=jsonpath='{.items[*].status.containerStatuses[0].ready}' | tr ' ' '\n' | sort | uniq)
+    PODS_READY=$(kubectl get pods -l app=flask-app -o=jsonpath='{.items[*].status.containerStatuses[0].ready}' | tr ' ' '\n' | sort | uniq)
     if [ "$PODS_READY" != "true" ]; then
         echo "Not all pods are ready. Retrying in 5 seconds..."
         sleep 5
@@ -16,7 +16,7 @@ check_pods_ready
 
 # Function to check if the LoadBalancer IP address is available
 check_loadbalancer_ip() {
-    CLUSTER_IP=$(kubectl get service docker-app-service -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    CLUSTER_IP=$(kubectl get service flask-app-service -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
     if [ -z "$CLUSTER_IP" ]; then
         echo "LoadBalancer IP not found. Retrying in 5 seconds..."
         sleep 5
